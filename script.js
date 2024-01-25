46f1f0d6b056bd81e079526d491cd6e9728874c5
PatriceKalwira <patricekalwira@gmail.com>
2024-01-25 21:49:58 +0200

    first commit

--- script.js
+++ script.js
@@ -17,18 +17,6 @@ const apiKey = '24ad9e8e90e09e00659e7d5290429afd';
                 document.querySelector('.temp').innerHTML = Math.round(data.main.temp) +'°c';
                 document.querySelector('.humidity').innerHTML = data.main.humidity +'%';
                 document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
-
-                // if (data.weather[0].main=='Clouds') {
-                //     weatherIcon.src = 'images/clouds.png';
-                // }else if (data.weather[0].main=='Clear') {
-                //     weatherIcon.src = 'images/clear.png';
-                // }else if (data.weather[0].main=='Rain') {
-                //     weatherIcon.src = 'images/rain.png';
-                // }else if (data.weather[0].main=='Drizzle') {
-                //     weatherIcon.src = 'images/drizzle.png';
-                // }else if (data.weather[0].main=='Mist') {
-                //     weatherIcon.src = 'images/mist.png';
-                // }
                 document.querySelector('.weather').style.display = 'block'
                 document.querySelector('.error').style.display = 'none'
             }
